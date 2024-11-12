80c28db446ce6c704b1e2a6b8eb2421b89bc031a
PYM5 <pymsoft5@gmail.com>
2024-11-13 00:05:07 +0100

    RAILS02-02 V1

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
+# Route pour afficher la liste des tâches (équivalent de tasks_path)
+get 'tasks', to: 'tasks#index', as: 'tasks'
+
+# Route pour créer une nouvelle tâche (équivalent de new_task_path)
+# placé avant task/:id car sinon new est considéré comme un id
+get 'tasks/new', to: 'tasks#new', as: 'new_task'
+post 'tasks', to: 'tasks#create'
+
+# Route pour afficher une tâche spécifique (équivalent de task_path)
+get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+
+# Route pour éditer une tâche existante (équivalent de edit_task_path)
+get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+patch 'tasks/:id', to: 'tasks#update'
+
+# Route pour supprimer une tâche (équivalent de task DELETE)
+delete 'tasks/:id', to: 'tasks#destroy'
+
 end
